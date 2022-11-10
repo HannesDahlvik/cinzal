ae4290f4972c8e313ca68b89df533f967b6ba5e8
HannesDahlvik <hannes.dahlvik@gmail.com>
2022-11-10 17:52:57 +0200

    Home timeline start

--- apps/mobile/src/screens/dashboard/Home.tsx
+++ apps/mobile/src/screens/dashboard/Home.tsx
@@ -1,13 +1,40 @@
+import { StyleSheet } from 'react-native'
+
 import TabScreenWrapper from '../../navigation/TabScreenWrapper'
 import { TabStackScreenProps } from '~/navigation/TabNavigator'
-import { Heading } from 'native-base'
+
+import { Box, ScrollView, Text, View } from 'native-base'
 
 const DashboardHomeScreen: React.FC<TabStackScreenProps<'DashboardHome'>> = () => {
+    const hours = Array.from<number>({ length: 24 }).fill(0)
+
     return (
         <TabScreenWrapper>
-            <Heading>Dashboard Home Screen</Heading>
+            <ScrollView>
+                {hours.map((_, hour) => (
+                    <View style={styles.hourBox} key={hour}>
+                        <Box p="1" borderWidth={1} borderColor="dark.400" width="60px">
+                            <Text>{hour}:00</Text>
+                        </Box>
+
+                        <Box p="1" borderWidth={1} borderColor="dark.400" width="100%" />
+                    </View>
+                ))}
+            </ScrollView>
         </TabScreenWrapper>
     )
 }
 
 export default DashboardHomeScreen
+
+const styles = StyleSheet.create({
+    wrapper: {
+        display: 'flex',
+        flexDirection: 'column'
+    },
+    hourBox: {
+        display: 'flex',
+        flexDirection: 'row',
+        height: 100
+    }
+})
